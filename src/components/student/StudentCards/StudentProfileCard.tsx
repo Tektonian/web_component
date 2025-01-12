8126f916731bc5e73676d1871446d95eed3ed582
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2025-01-12 21:15:58 +0900

    학생 프로필 카드 수정

--- src/components/student/StudentCards/StudentProfileCard.tsx
+++ src/components/student/StudentCards/StudentProfileCard.tsx
@@ -1,63 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { Card, CardMedia } from "@mui/material";
 import StudentProfileCardContent from "./StudentCardContent/StudentProfileCardContent";
-import StudentProfileInputCardContent from "./StudentCardContent/StudentProfileInputCardContent";
 import * as EditableCard from "./../..//input/EditableCard";
-import EditableCardHeader from "../../input/EditableCardHeader";
 import "@fontsource/noto-sans-kr";
 
 import type { Control } from "react-hook-form";
 import { APIType } from "api_spec";
 interface StudentProfileCardProps
-    extends APIType.StudentType.StudentProfileData {
-    isMyPage?: boolean;
-    isEditMode?: boolean;
-    onClickSaveIcon?: React.MouseEventHandler;
+    extends APIType.StudentType.StudentProfileData<false> {
+    onClickCard?: React.MouseEventHandler;
 }
 
 const StudentProfileCard = ({
-    isMyPage = false,
-    isEditMode = false,
-    onClickSaveIcon,
+    onClickCard,
     ...defaultValues
 }: StudentProfileCardProps) => {
-    const [editMode, setEditMode] = useState(isEditMode);
-    const [dirted, setDirted] = useState(false);
-
-    const { control } = useForm<
-        Omit<APIType.StudentType.StudentProfileData, "student_id">
-    >({
-        defaultValues: defaultValues,
-    });
-
-    useEffect(() => {
-        console.log(control);
-    }, [control._formState.isDirty]);
     return (
-        <EditableCard.EditableCard
-            editMode={editMode}
-            header={
-                <EditableCardHeader
-                    disabled={!isMyPage}
-                    editMode={editMode}
-                    onClickEditIcon={() => setEditMode(true)}
-                    onClickCloseIcon={() => setEditMode(false)}
-                    onClickSaveIcon={onClickSaveIcon}
-                />
-            }
-        >
+        <EditableCard.EditableCard header={undefined}>
             <EditableCard.NormalMode>
                 <StudentProfileCardContent
                     {...defaultValues}
-                    isMyPage={isMyPage}
+                    isMyPage={false}
                 />
             </EditableCard.NormalMode>
-            <EditableCard.EditMode>
-                <StudentProfileInputCardContent
-                    {...defaultValues}
-                    control={control}
-                />
-            </EditableCard.EditMode>
         </EditableCard.EditableCard>
     );
 };
